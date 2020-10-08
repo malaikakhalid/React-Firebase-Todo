996e4d3cab9c98ad3e4552d13394ec1b2c448717
malaikakhalid <70518776+malaikakhalid@users.noreply.github.com>
2020-10-08 21:00:59 +0500

    Updates

---
+++ precache-manifest.67bf2eccea19845f8a66bd79854253c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcdb8a8d6162e300aeba8e5fbab68c7",
+    "url": "/React-Firebase-Todo/index.html"
+  },
+  {
+    "revision": "58360d031757fa56e295",
+    "url": "/React-Firebase-Todo/static/css/main.4717d4a5.chunk.css"
+  },
+  {
+    "revision": "8aa7aa7116d7fdcb9907",
+    "url": "/React-Firebase-Todo/static/js/2.ec7481ea.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/React-Firebase-Todo/static/js/2.ec7481ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58360d031757fa56e295",
+    "url": "/React-Firebase-Todo/static/js/main.baa8f94b.chunk.js"
+  },
+  {
+    "revision": "9bec5a5becdbc5957839",
+    "url": "/React-Firebase-Todo/static/js/runtime-main.d4acf6c8.js"
+  },
+  {
+    "revision": "866f89919546ea3f0274f4bdb627d905",
+    "url": "/React-Firebase-Todo/static/media/istockphoto-509927696-1024x1024.866f8991.jpg"
+  }
+]);
